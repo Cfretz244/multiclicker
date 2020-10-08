26f0cadf2cbd0796e465ec0884a7be86aa7e547f
Chris Fretz <cfretz@icloud.com>
2020-10-08 22:13:21 +0000

    added redis authentication

--- client.rb
+++ client.rb
@@ -8,19 +8,20 @@ require 'optimist'
 
 opts = Optimist.options do
   opt :redis_host, 'The host to connect to redis on', default: '127.0.0.1'
-  opt :redis_port, 'The port to connect to redis on ', default: 7024
+  opt :redis_port, 'The port to connect to redis on', default: 7024
+  opt :redis_pass, 'The password to use with redis', default: 'hai_cpp_guild'
 end
 
 #----- Setup -----#
 
 # Get redis handle
-redis = Redis.new(host: opts[:redis_host], port: opts[:redis_port])
+redis = Redis.new(host: opts[:redis_host], port: opts[:redis_port], password: opt[:redis_pass])
 
 #----- Application Logic -----#
 
 redis.subscribe('multiclicker') do |on|
   on.message do |_, msg|
     cmd = JSON.parse(msg)
-    puts "Performing #{cmd['action']} click"
+    puts "Performing #{cmd['key']} click"
   end
 end
--- server.rb
+++ server.rb
@@ -10,7 +10,8 @@ require 'optimist'
 
 opts = Optimist.options do
   opt :redis_host, 'The host to connect to redis on', default: '127.0.0.1'
-  opt :redis_port, 'The port to connect to redis on ', default: 7024
+  opt :redis_port, 'The port to connect to redis on', default: 7024
+  opt :redis_pass, 'The password to use with redis', default: 'hai_cpp_guild'
   opt :http_port, 'The port to accept HTTP connections on', default: 8080
 end
 
@@ -20,7 +21,7 @@ set :port, opts[:http_port]
 #----- Setup -----#
 
 # Get redis handle
-redis = Redis.new(host: opts[:redis_host], port: opts[:redis_port])
+redis = Redis.new(host: opts[:redis_host], port: opts[:redis_port], password: opts[:redis_pass])
 
 #----- Application Logic -----#
 
@@ -31,5 +32,5 @@ end
 
 post '/action' do
   # Key pressed, forward over redis
-  redis.publish('multiclicker', {action: params[:key]}.to_json)
+  redis.publish('multiclicker', params.to_json)
 end
